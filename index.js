225a48ed8117a6fa25a2a79b3b74ee6e9e56f517
DaniGM17 <daniagm75@gmail.com>
2022-11-17 21:44:33 -0600

    Project setup

---
+++ index.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCic9rtMLKkGUiImKzFU5iwmVWgnlBpG6E",
+    authDomain: "fir-javascript-crud-c5564.firebaseapp.com",
+    projectId: "fir-javascript-crud-c5564",
+    storageBucket: "fir-javascript-crud-c5564.appspot.com",
+    messagingSenderId: "436796516225",
+    appId: "1:436796516225:web:f4061da6e890f5ddbfea80"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
