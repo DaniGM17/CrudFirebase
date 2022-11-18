4d1e833262f9cad867f652f6c2c0603672b93e05
DaniGM17 <daniagm75@gmail.com>
2022-11-17 22:14:47 -0600

    Firestore setup

---
+++ firebase.js
@@ -0,0 +1,24 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCic9rtMLKkGUiImKzFU5iwmVWgnlBpG6E",
+    authDomain: "fir-javascript-crud-c5564.firebaseapp.com",
+    projectId: "fir-javascript-crud-c5564",
+    storageBucket: "fir-javascript-crud-c5564.appspot.com",
+    messagingSenderId: "436796516225",
+    appId: "1:436796516225:web:f4061da6e890f5ddbfea80"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore();
+
+  export const saveTask = (title, description) => {
+    addDoc(collection(db, 'tasks'), {title, description});
+  }
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import {saveTask} from './firebase.js';
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCic9rtMLKkGUiImKzFU5iwmVWgnlBpG6E",
-    authDomain: "fir-javascript-crud-c5564.firebaseapp.com",
-    projectId: "fir-javascript-crud-c5564",
-    storageBucket: "fir-javascript-crud-c5564.appspot.com",
-    messagingSenderId: "436796516225",
-    appId: "1:436796516225:web:f4061da6e890f5ddbfea80"
-  };
+window.addEventListener('DOMContentLoaded', () => {
+    
+});
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const taskForm = document.getElementById('task-form');
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Submitted');
+
+    const title = taskForm['task-title'];
+    const description = taskForm['task-description'];
+
+    saveTask(title.value, description.value);
+
+    taskForm.reset();
+});
